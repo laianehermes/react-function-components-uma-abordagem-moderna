6b6ea48accc3776e1e97ced0d9ac74211cd7fd0c
lsouza <lsouza@luxfacta.com>
2021-07-05 15:00:30 -0300

    use hooks in registration form

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@material-ui/core";
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [cpf, setCpf] = useState("");
+  //   const [promocoes, setPromocoes] = useState("");
+  //   const [novidades, setNovidades] = useState("");
+
   return (
     <>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          console.log(nome, sobrenome);
+        }}
+      >
         <TextField
+          value={nome}
+          onChange={(event) => {
+            let tempNome = event.target.value;
+
+            if (tempNome.length >= 3) {
+              tempNome = tempNome.substr(0, 3);
+            }
+            setNome(tempNome);
+          }}
           id='nome'
           label='Nome'
           variant='outlined'
@@ -13,6 +33,10 @@ function FormularioCadastro() {
           fullWidth
         />
         <TextField
+          value={sobrenome}
+          onChange={(event) => {
+            setSobrenome(event.target.value);
+          }}
           id='sobrenome'
           label='Sobrenome'
           variant='outlined'
@@ -20,6 +44,10 @@ function FormularioCadastro() {
           fullWidth
         />
         <TextField
+          value={cpf}
+          onChange={(event) => {
+            setCpf(event.target.value);
+          }}
           id='cpf'
           label='CPF'
           variant='outlined'
